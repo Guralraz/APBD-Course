e682ce2e38673ae51fb44451adf917ff2a84f7b8
Guralraz <87212802+Guralraz@users.noreply.github.com>
2024-03-24 23:54:34 +0100

    Dodano statyczną metodę obliczającą średnią

--- Console App/Program.cs
+++ Console App/Program.cs
@@ -3,3 +3,16 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+
+static double ObliczSrednia(int[] liczby)
+{
+    if (liczby.Length == 0)
+        return 0;
+
+    double suma = 0;
+    foreach (int liczba in liczby)
+    {
+        suma += liczba;
+    }
+    return suma / liczby.Length;
+}
